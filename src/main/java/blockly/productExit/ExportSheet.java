76cb93fe9469438e546ddcec71318fddfba01143
Andre Lucio Rocha Wanderley <andre.wanderley@techne.com.br>
2025-05-16 16:23:11 +0000

    feat: exportação de saidas em csv

---
+++ src/main/java/blockly/productExit/ExportSheet.java
@@ -0,0 +1,85 @@
+package blockly.productExit;
+
+import cronapi.*;
+import cronapi.rest.security.CronappSecurity;
+import java.util.Iterator;
+import java.util.concurrent.Callable;
+
+
+@CronapiMetaData(type = "blockly")
+@CronappSecurity
+public class ExportSheet {
+
+public static final int TIMEOUT = 300;
+
+/**
+ *
+ * @author Andre Lucio Rocha Wanderley
+ * @since 16/05/2025, 13:19:26
+ *
+ */
+public static Var exportCsv() throws Exception {
+ return new Callable<Var>() {
+
+   private Var filePath = Var.VAR_NULL;
+   private Var productExits = Var.VAR_NULL;
+   private Var file2 = Var.VAR_NULL;
+   private Var p = Var.VAR_NULL;
+   private Var e = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+    try {
+         filePath =
+        Var.valueOf(
+        cronapi.io.Operations.fileAppReclycleDir().getObjectAsString() +
+        cronapi.io.Operations.fileSeparator().getObjectAsString() +
+        Var.valueOf("file.csv").getObjectAsString());
+        cronapi.io.Operations.fileCreate(filePath);
+        productExits =
+        cronapi.database.Operations.query(Var.valueOf("app.entity.ProductExit"),Var.valueOf("select \n	p \nfrom \n	ProductExit p"));
+        file2 =
+        cronapi.io.Operations.fileOpenToWrite(filePath,
+        Var.valueOf(
+        Var.valueOf("id,registeringUser,product,amount,date").getObjectAsString() +
+        cronapi.text.Operations.newline().getObjectAsString()));
+        for (Iterator it_p = productExits.iterator(); it_p.hasNext();) {
+            p = Var.valueOf(it_p.next());
+            cronapi.io.Operations.fileAppend(file2,
+            Var.valueOf(
+            cronapi.json.Operations.getJsonOrMapField(p,
+            Var.valueOf("id")).getObjectAsString() +
+            Var.valueOf(",").getObjectAsString() +
+            cronapi.json.Operations.getJsonOrMapField(
+            cronapi.json.Operations.getJsonOrMapField(p,
+            Var.valueOf("registeringUser")),
+            Var.valueOf("id")).getObjectAsString() +
+            Var.valueOf(",").getObjectAsString() +
+            cronapi.json.Operations.getJsonOrMapField(
+            cronapi.json.Operations.getJsonOrMapField(p,
+            Var.valueOf("product")),
+            Var.valueOf("id")).getObjectAsString() +
+            Var.valueOf(",").getObjectAsString() +
+            cronapi.json.Operations.getJsonOrMapField(p,
+            Var.valueOf("amount")).getObjectAsString() +
+            Var.valueOf(",").getObjectAsString() +
+            cronapi.json.Operations.getJsonOrMapField(p,
+            Var.valueOf("date")).getObjectAsString() +
+            cronapi.text.Operations.newline().getObjectAsString()));
+        } // end for
+        cronapi.io.Operations.fileClose(file2);
+        cronapi.io.Operations.startDownload(filePath,
+        Var.valueOf("saidas.csv"));
+     } catch (Exception e_exception) {
+          e = Var.valueOf(e_exception);
+         cronapi.util.Operations.throwException(
+        cronapi.util.Operations.createException(
+        Var.valueOf("Erro ao gerar o CSV de saídas a serem exportadas.")));
+     }
+    return
+cronapi.util.Operations.createDownloadLink(file2);
+   }
+ }.call();
+}
+
+}
+
