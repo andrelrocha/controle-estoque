abe1723a1f332acb970627d149c582b04097e669
Andre Lucio Rocha Wanderley <andre.wanderley@techne.com.br>
2025-05-21 14:57:28 +0000

    refactor: modularizando a criação de produto

--- src/main/webapp/js/blockly/product/CreateProduct.blockly.js
+++ src/main/webapp/js/blockly/product/CreateProduct.blockly.js
@@ -11,7 +11,7 @@ window.blockly.js.blockly.product.CreateProduct = window.blockly.js.blockly.prod
  *
  *
  * @author Andre Lucio Rocha Wanderley
- * @since 21/05/2025, 11:45:43
+ * @since 21/05/2025, 11:55:39
  *
  */
 window.blockly.js.blockly.product.CreateProduct.runArgs = [];
@@ -22,14 +22,14 @@ window.blockly.js.blockly.product.CreateProduct.run = async function() {
      //
     objData = (await this.cronapi.client('blockly.js.blockly.product.BuildObject.buildCreateProduct').run());
     //
-    this.cronapi.util.callServerBlocklyAsynchronous('blockly.productEntry.AddProductEntry:save', async function(sender_serverResponse) {
+    this.cronapi.util.callServerBlocklyAsynchronous('blockly.product.CreateProduct:createFromJSON', async function(sender_serverResponse) {
         serverResponse = sender_serverResponse;
       //
-      this.cronapi.screen.notify('success','Entrada do produto adicionada com sucesso no sistema!');
+      this.cronapi.screen.notify('success','Produto adicionado com sucesso no sistema!');
       //
-      this.cronapi.screen.refreshDatasource("ProductEntry", 'true');
+      this.cronapi.screen.refreshDatasource("Product", 'true');
       //
-      (await this.cronapi.client('blockly.js.blockly.productEntry.ModalHandler.closeAddModal').run());
+      (await this.cronapi.client('blockly.js.blockly.product.ModalHandler.closeAddModal').run());
     }.bind(this), objData);
    } catch (e_exception) {
         e = e_exception;
