c683339f901937ece08718f791f092a6be2d1484
Andre Lucio Rocha Wanderley <andre.wanderley@techne.com.br>
2025-05-27 15:41:33 +0000

    refactor: fluxo cliente servidor, com retorno por mapa, do status do download do csv dos produtos

--- src/main/java/blockly/product/ExportSheet.java
+++ src/main/java/blockly/product/ExportSheet.java
@@ -4,6 +4,7 @@ import cronapi.*;
 import cronapi.rest.security.CronappSecurity;
 import java.util.Iterator;
 import java.util.concurrent.Callable;
+import org.springframework.web.bind.annotation.*;
 
 
 @CronapiMetaData(type = "blockly")
@@ -15,16 +16,14 @@ public static final int TIMEOUT = 300;
 /**
  *
  * @author Andre Lucio Rocha Wanderley
- * @since 21/05/2025, 10:18:56
+ * @since 27/05/2025, 12:40:51
  *
  */
 public static Var exportCsv() throws Exception {
  return new Callable<Var>() {
 
    private Var filePath = Var.VAR_NULL;
-   private Var products = Var.VAR_NULL;
-   private Var file2 = Var.VAR_NULL;
-   private Var p = Var.VAR_NULL;
+   private Var response = Var.VAR_NULL;
    private Var e = Var.VAR_NULL;
 
    public Var call() throws Exception {
@@ -33,15 +32,51 @@ public static Var exportCsv() throws Exception {
         Var.valueOf(
         cronapi.io.Operations.fileAppReclycleDir().getObjectAsString() +
         cronapi.io.Operations.fileSeparator().getObjectAsString() +
-        Var.valueOf("file.csv").getObjectAsString());
-        cronapi.io.Operations.fileCreate(filePath);
-        products =
-        cronapi.database.Operations.query(Var.valueOf("app.entity.Product"),Var.valueOf("select \n	p \nfrom \n	Product p"));
-        file2 =
+        Var.valueOf("produtos.csv").getObjectAsString());
+        writeDataToCsv(filePath);
+        cronapi.io.Operations.startDownload(filePath,
+        Var.valueOf("produtos.csv"));
+        response =
+        cronapi.map.Operations.createObjectMapWith(Var.valueOf("success",
+        Var.VAR_TRUE) , Var.valueOf("message",
+        Var.valueOf("Download do .csv realizado com sucesso!")));
+     } catch (Exception e_exception) {
+          e = Var.valueOf(e_exception);
+         response =
+        cronapi.map.Operations.createObjectMapWith(Var.valueOf("success",
+        Var.VAR_FALSE) , Var.valueOf("message",
+        cronapi.util.Operations.getExceptionMessage(e)));
+     }
+    return response;
+   }
+ }.call();
+}
+
+/**
+ *
+ * @param filePath
+ *
+ * @author Andre Lucio Rocha Wanderley
+ * @since 27/05/2025, 12:40:51
+ *
+ */
+public static void writeDataToCsv(@ParamMetaData(description = "filePath", id = "2b79a2f4") @RequestBody(required = false) Var filePath) throws Exception {
+  new Callable<Var>() {
+
+   private Var e = Var.VAR_NULL;
+   private Var file2 = Var.VAR_NULL;
+   private Var products = Var.VAR_NULL;
+   private Var p = Var.VAR_NULL;
+
+   public Var call() throws Exception {
+    try {
+         file2 =
         cronapi.io.Operations.fileOpenToWrite(filePath,
         Var.valueOf(
         Var.valueOf("id,name,amount,minQuantity,maxQuantity").getObjectAsString() +
         cronapi.text.Operations.newline().getObjectAsString()));
+        products =
+        cronapi.util.Operations.callBlockly(Var.valueOf("blockly.product.GetProduct:getAll"));
         for (Iterator it_p = products.iterator(); it_p.hasNext();) {
             p = Var.valueOf(it_p.next());
             cronapi.io.Operations.fileAppend(file2,
@@ -63,16 +98,13 @@ public static Var exportCsv() throws Exception {
             cronapi.text.Operations.newline().getObjectAsString()));
         } // end for
         cronapi.io.Operations.fileClose(file2);
-        cronapi.io.Operations.startDownload(filePath,
-        Var.valueOf("produtos.csv"));
      } catch (Exception e_exception) {
           e = Var.valueOf(e_exception);
          cronapi.util.Operations.throwException(
         cronapi.util.Operations.createException(
-        Var.valueOf("Erro ao gerar o CSV de produtos a serem exportados.")));
+        Var.valueOf("Erro ao escrever os dados dos produtos no CSV a ser exportado.")));
      }
-    return
-cronapi.util.Operations.createDownloadLink(file2);
+   return Var.VAR_NULL;
    }
  }.call();
 }
